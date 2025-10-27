aa02eb0a90fb3c7fe34ba646ef6e15a0304096e5
Benjamin Grolleau <127044450+Grolleau-Benjamin@users.noreply.github.com>
2025-10-27 21:09:31 +0100

    delete: clear files containing dummy tests

--- src/cli/args.rs
+++ src/cli/args.rs
@@ -1,13 +1,1 @@
-pub fn get_hello() -> &'static str {
-    "Hello, world!"
-}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_get_hello_correct_output() {
-        assert_eq!(get_hello(), "Hello, world!");
-    }
-}
