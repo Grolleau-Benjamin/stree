5dc32becdc3c3765eba60be790524e5a5b3a731b
Benjamin Grolleau <127044450+Grolleau-Benjamin@users.noreply.github.com>
2025-10-30 01:06:53 +0100

    refactor: add SPDX headers and adjust version metadata across modules

--- src/fs_scan/walk.rs
+++ src/fs_scan/walk.rs
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: Apache-2.0
+
+// Copyright (c) 2025 Benjamin Grolleau and Angelo Tunney
+
 use ignore::{DirEntry, WalkBuilder};
 use std::{
     collections::HashMap,
--- src/model/node.rs
+++ src/model/node.rs
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: Apache-2.0
+
+// Copyright (c) 2025 Benjamin Grolleau and Angelo Tunney
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Kind {
     Dir,
